3d69cdd708bd1f7a10e72a0276e78decdc40a37f
Vinicius <Vinicius@VINICIUS-PC>
2021-06-15 23:41:04 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AULAPROGWEB.Models
+{
+    namespace Projeto01.Models
+    {
+        public class Categoria
+        {
+            public long CategoriaId { get; set; }
+            public string Nome { get; set; }
+        }
+    }
+}
